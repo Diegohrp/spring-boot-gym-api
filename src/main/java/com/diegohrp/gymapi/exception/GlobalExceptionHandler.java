147c3cb251715030ca6aa5e21a5bf383e072922c
diegohp <diegohp.dev@gmail.com>
2024-11-27 22:32:33 -0600

    refactor(exceptions): Add handle client exceptions

--- src/main/java/com/diegohrp/gymapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/diegohrp/gymapi/exception/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.diegohrp.gymapi.exception;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 
 
@@ -40,14 +40,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleNotFoundException(EntityNotFoundException ex, WebRequest request) {
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<Map<String, Object>> handleClientErrors(HttpClientErrorException ex, WebRequest request) {
         Map<String, Object> response = new HashMap<>();
         response.put("timestamp", LocalDateTime.now());
-        response.put("status", HttpStatus.NOT_FOUND.value());
-        response.put("error", "Not Found");
-        response.put("message", ex.getMessage());
+        response.put("status", ex.getStatusCode().value());
+        response.put("error", ex.getStatusCode().toString());
+        response.put("message", ex.getMessage().substring(4));
         response.put("path", request.getDescription(false).substring(4));
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
+
+
 }
