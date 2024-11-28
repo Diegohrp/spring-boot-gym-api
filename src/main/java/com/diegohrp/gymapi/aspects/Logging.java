6b0d6e300eda81257f65ec68d533d29f3203111a
diegohp <diegohp.dev@gmail.com>
2024-11-27 22:30:47 -0600

    fix(aspects): Hide passwords in logging

--- src/main/java/com/diegohrp/gymapi/aspects/Logging.java
+++ src/main/java/com/diegohrp/gymapi/aspects/Logging.java
@@ -1,14 +1,12 @@
 package com.diegohrp.gymapi.aspects;
 
+import com.diegohrp.gymapi.dto.user.ChangePasswordDto;
+import com.diegohrp.gymapi.dto.user.LoginUserDto;
 import com.diegohrp.gymapi.dto.user.UserCreatedDto;
 import jakarta.servlet.http.HttpServletRequest;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,14 +14,13 @@ import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.UUID;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 
 @Aspect
@@ -36,10 +33,11 @@ public class Logging {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         String transactionId = UUID.randomUUID().toString();
         MDC.put("transactionId", transactionId);
+
         String enpoint = request.getRequestURI();
         String requestMethod = request.getMethod();
-        String body = getRequestBody(joinPoint);
-        getRequestBody(joinPoint);
+        String body = getRequestBody(joinPoint).toString();
+
         logger.info("Transaction ID: {}, Received request: [Method: {}] [Endpoint: {}] [Body: {}]", transactionId, requestMethod, enpoint, body);
 
         try {
@@ -51,7 +49,11 @@ public class Logging {
             logger.info("Transaction ID: {}, Completed request: Status: {}, Body: {}", transactionId, result.getStatusCode(), resp);
             return result;
         } catch (Throwable e) {
-            logger.error("Transaction ID: {}, Error in request", transactionId);
+            if (e instanceof HttpClientErrorException) {
+                logger.warn("Transaction ID: {}, Client Error: {}", transactionId, e.getMessage());
+            } else {
+                logger.error("Transaction ID: {}, Error in request", transactionId);
+            }
             throw new RuntimeException(e);
 
         } finally {
@@ -66,20 +68,30 @@ public class Logging {
 
         logger.info("Transaction ID: {}, starts calling: {}.{}", transactionId, method.getDeclaringClass().getSimpleName(), method.getName());
         Object result = joinPoint.proceed();
-        logger.info("Transaction ID: {}, ended calling: {}", transactionId, method);
+        logger.info("Transaction ID: {}, ended calling: {}.{}", transactionId, method.getDeclaringClass().getSimpleName(), method.getName());
         return result;
     }
 
-    private String getRequestBody(ProceedingJoinPoint joinPoint) {
+    private Object getRequestBody(ProceedingJoinPoint joinPoint) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod(); // Verify method's params
         Object[] args = joinPoint.getArgs();
         // Search for the param annotated with @RequestBody and get its value
-        String requestBody = IntStream.range(0, method.getParameterCount())
+        Object requestBody = IntStream.range(0, method.getParameterCount())
                 .filter(i -> method.getParameters()[i].isAnnotationPresent(RequestBody.class))
-                .mapToObj(i -> args[i] != null ? args[i].toString() : "null")
+                .mapToObj(i -> args[i] != null ? args[i] : "null")
                 .findFirst()
                 .orElse("No Body");
+        //Remove sensitive fields
+        if (requestBody instanceof LoginUserDto) {
+            requestBody = new LoginUserDto(((LoginUserDto) requestBody).username(), "**********");
+        }
+        if (requestBody instanceof ChangePasswordDto) {
+            requestBody = new ChangePasswordDto(
+                    ((ChangePasswordDto) requestBody).username(),
+                    "**********",
+                    "**********");
+        }
         return requestBody;
     }
 
