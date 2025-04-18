1520668861a60687ea68d93c2aaacc364246542d
Diego Herrera <diegohp.dev@gmail.com>
2025-04-17 23:04:10 -0600

    feat(TrainerWorkloadClient): send transactionId to the external microservice

--- src/main/java/com/diegohrp/gymapi/client/TrainerWorkloadClient.java
+++ src/main/java/com/diegohrp/gymapi/client/TrainerWorkloadClient.java
@@ -4,8 +4,13 @@ import com.diegohrp.gymapi.dto.trainer.TrainerWorkloadDto;
 import com.diegohrp.gymapi.logging.LoggingService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
+import org.slf4j.MDC;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 
 @Service
 @AllArgsConstructor
@@ -15,9 +20,14 @@ public class TrainerWorkloadClient {
 
     @CircuitBreaker(name = "trainingHoursService", fallbackMethod = "fallbackSendWorkload")
     public void send(TrainerWorkloadDto workloadDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("X-Transaction-Id", MDC.get("transactionId"));
+        HttpEntity<TrainerWorkloadDto> request = new HttpEntity<>(workloadDto, headers);
+
         restTemplate.postForEntity(
                 "http://training-hours-service/training-hours/api/v1/trainers/training-hours",
-                workloadDto,
+                request,
                 Void.class
         );
     }
